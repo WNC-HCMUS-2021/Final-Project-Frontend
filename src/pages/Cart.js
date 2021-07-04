9aed720482f48fb2410cac51f6cc5f4151b9744e
Hoang Pham <hoangpm6814@gmail.com>
2021-07-04 20:43:05 +0700

    Load Data To Cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,8 +3,12 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserAlt, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from "react-redux";
+import MessageBox from "../components/MessageBox";
 
 function Cart() {
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart; 
     return (
         <>
             <section style={{height: "100px", width: "100%", display: "flex", alignItems: "center", background: "black"}}>
@@ -13,165 +17,102 @@ function Cart() {
                 </div>
             </section>
             <Container>
-                <Row className="mt-5">
-                    <Col style={{textAlign: "left"}}>
-                        <h5>3 Course(s) in Cart</h5>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={9}>
-                        <div className="card ">
-                            <div className="card-body">
+                {
+                    cartItems.length === 0 
+                    ? 
+                    (
+                        <MessageBox>
+                            Cart is now empty!!! {" "}
+                            <Link to="/">Back to home to shop</Link>
+                        </MessageBox>
+                    )
+                    :
+                    (
+                        <>
+                        <Row className="mt-5">
+                            <Col style={{textAlign: "left"}}>
+                                <h5>{cartItems.length} Course(s) in Cart</h5>
+                            </Col>
+                        </Row>
+                        
+                            
                                 <Row>
-                                <Col xs={3}>
-                                    {/* <Image src="https://i.ibb.co/ch8dHPv/4.jpg" thumbnail /> */}
-                                    <Link to="/coursedetail">
-                                        <img className="card-img"
-                                            src="https://i.ibb.co/85Ssw1y/1.png"
-                                            style={{maxWidth: "150px", maxHeight: "100px"}}
-                                            alt="" />
-                                    </Link>
-                                </Col>
-                                <Col xs={4} style={{textAlign: "left"}}>
-                                    <div>
-                                        <Link to="/coursedetail" style={{ color: 'black' }}>
-                                        <b className="card-title">
-                                            React with Redux
-                                        </b>
-                                        </Link>
-                                    </div>
-                                    <div className="instructor">
-                                        <FontAwesomeIcon
-                                        className="user-icon"
-                                        icon={faUserAlt} />
-                                        Stephen Grider
-                                    </div>
-                                </Col>
-                                <Col xs={3}>
-                                    <Link to="/">
-                                        <p>Remove From Cart</p>
-                                    </Link>
-                                </Col>
-                                <Col xs={2} className="price-course-list">
-                                    <div className="price-discount-course-list">
-                                        $139.99
-                                    </div>
-                                    <div className="price-original-course-list">
-                                        $199.99
-                                    </div>
-                                </Col>
+                                    <Col xs={9}>
+                                        {
+                                            cartItems.map((item) => 
+                                            (
+                                                <div className="card" key={item.academy}>
+                                                    <div className="card-body">
+                                                        <Row>
+                                                            <Col xs={3}>
+                                                                {/* <Image src="https://i.ibb.co/ch8dHPv/4.jpg" thumbnail /> */}
+                                                                <Link to={`/coursedetail/${item.academy}`}>
+                                                                    <img className="card-img"
+                                                                        src={item.avatar}
+                                                                        style={{maxWidth: "150px", maxHeight: "100px"}}
+                                                                        alt="" />
+                                                                </Link>
+                                                            </Col>
+                                                            <Col xs={4} style={{textAlign: "left"}}>
+                                                                <div>
+                                                                    <Link to={`/coursedetail/${item.academy}`} style={{ color: 'black' }}>
+                                                                    <b className="card-title">
+                                                                        {item.name}
+                                                                    </b>
+                                                                    </Link>
+                                                                </div>
+                                                                <div className="instructor">
+                                                                    <FontAwesomeIcon
+                                                                    className="user-icon"
+                                                                    icon={faUserAlt} />
+                                                                    Stephen Grider
+                                                                </div>
+                                                            </Col>
+                                                            <Col xs={3}>
+                                                                <Link to="/">
+                                                                    <p>Remove From Cart</p>
+                                                                </Link>
+                                                            </Col>
+                                                            <Col xs={2} className="price-course-list">
+                                                                <div className="price-discount-course-list">
+                                                                    ${item.price_discount}
+                                                                </div>
+                                                                <div className="price-original-course-list">
+                                                                    ${item.price}
+                                                                </div>
+                                                            </Col>
+                                                        </Row>
+                                                    </div>
+                                                </div>
+                                            ))
+                                        }
+                                    </Col>
+                                    <Col>
+                                        <div className="card card-body card border-top-0 border-right-0 border-left-0 border-bottom-0">
+                                            <Row>
+                                                <h5>
+                                                    Total:
+                                                </h5>
+                                            </Row>
+                                            <Row>
+                                                <h2>
+                                                    ${cartItems.reduce((accumulator, currentValue) => accumulator + currentValue.price_discount, 0)}
+                                                </h2>
+                                            </Row>
+                                            <Row>
+                                                <button className="btn btn-success" style={{width: "200px"}}>
+                                                    <FontAwesomeIcon icon={faCheckSquare} /> Buy Course(s)
+                                                </button>
+                                            </Row>
+                                        </div>
+                                    </Col>
                                 </Row>
-                            </div>
-                        </div>
-                        <div className="card ">
-                            <div className="card-body">
-                                <Row>
-                                <Col xs={3}>
-                                    {/* <Image src="https://i.ibb.co/ch8dHPv/4.jpg" thumbnail /> */}
-                                    <Link to="/coursedetail">
-                                        <img className="card-img"
-                                            src="https://i.ibb.co/85Ssw1y/1.png"
-                                            style={{maxWidth: "150px", maxHeight: "100px"}}
-                                            alt="" />
-                                    </Link>
-                                </Col>
-                                <Col xs={4} style={{textAlign: "left"}}>
-                                    <div>
-                                        <Link to="/coursedetail" style={{ color: 'black' }}>
-                                        <b className="card-title">
-                                            React with Redux
-                                        </b>
-                                        </Link>
-                                    </div>
-                                    <div className="instructor">
-                                        <FontAwesomeIcon
-                                        className="user-icon"
-                                        icon={faUserAlt} />
-                                        Stephen Grider
-                                    </div>
-                                </Col>
-                                <Col xs={3}>
-                                    <Link to="/">
-                                        <p>Remove Form Cart</p>
-                                    </Link>
-                                </Col>
-                                <Col xs={2} className="price-course-list">
-                                    <div className="price-discount-course-list">
-                                        $139.99
-                                    </div>
-                                    <div className="price-original-course-list">
-                                        $199.99
-                                    </div>
-                                </Col>
-                                
-                                </Row>
-                            </div>
-                        </div>
-                        <div className="card ">
-                            <div className="card-body">
-                                <Row>
-                                <Col xs={3}>
-                                    {/* <Image src="https://i.ibb.co/ch8dHPv/4.jpg" thumbnail /> */}
-                                    <Link to="/coursedetail">
-                                        <img className="card-img"
-                                            src="https://i.ibb.co/85Ssw1y/1.png"
-                                            style={{maxWidth: "150px", maxHeight: "100px"}}
-                                            alt="" />
-                                    </Link>
-                                </Col>
-                                <Col xs={4} style={{textAlign: "left"}}>
-                                    <div>
-                                        <Link to="/coursedetail" style={{ color: 'black' }}>
-                                        <b className="card-title">
-                                            React with Redux
-                                        </b>
-                                        </Link>
-                                    </div>
-                                    <div className="instructor">
-                                        <FontAwesomeIcon
-                                        className="user-icon"
-                                        icon={faUserAlt} />
-                                        Stephen Grider
-                                    </div>
-                                </Col>
-                                <Col xs={3}>
-                                    <Link to="/">
-                                        <p>Remove Form Cart</p>
-                                    </Link>
-                                </Col>
-                                <Col xs={2} className="price-course-list">
-                                    <div className="price-discount-course-list">
-                                        $139.99
-                                    </div>
-                                    <div className="price-original-course-list">
-                                        $199.99
-                                    </div>
-                                </Col>
-                                
-                                </Row>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="card card-body card border-top-0 border-right-0 border-left-0 border-bottom-0">
-                            <Row>
-                                <h5>
-                                    Total:
-                                </h5>
-                            </Row>
-                            <Row>
-                                <h2>
-                                    $199.99
-                                </h2>
-                            </Row>
-                            <Row>
-                                <button className="btn btn-success" style={{width: "200px"}}>
-                                    <FontAwesomeIcon icon={faCheckSquare} /> Buy Course(s)
-                                </button>
-                            </Row>
-                        </div>
-                    </Col>
-                </Row>
+                            
+                        
+                        </>
+                    )
+                }
+                
             </Container>
         </>
     );
